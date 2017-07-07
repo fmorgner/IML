9c033e03c888dc8a99e814de77d54be4b32ab9e7
Felix Morgner <felix.morgner@gmail.com>
2017-07-08 01:42:08 +0200

    doc: add example program

--- README.rst
+++ README.rst
@@ -1,5 +1,31 @@
 IML
----
+===
 
 This is an experiment on implementing an interpreter for a dialect of the
 Imperative Mini-Language (IML).
+
+Example Program
+---------------
+
+The following code:
+
+.. code-block:: text
+
+  quo <- 0
+  rem <- 42
+  while(rem >= 6) {
+    quo <- (quo + 1)
+    rem <- (rem - 6)
+  }
+
+Produces the following (prettified) token stream:
+
+.. code-block:: text
+
+  IDENTIFIER quo, BECOMES, NUMERIC 0,
+  IDENTIFIER rem, BECOMES, NUMERIC 42,
+  WHILE, LEFTPAREN, IDENTIFIER rem, RELATIONALOPERATOR GreaterThanOrEqual, NUMERIC 6, LEFTCURLY,
+    IDENTIFIER quo, BECOMES, LEFTPAREN, IDENTIFIER quo, ARITHMETICOPERATOR Plus, NUMERIC 1,
+    IDENTIFIER rem, BECOMES, LEFTPAREN, IDENTIFIER rem, ARITHMETICOPERATOR Minus, NUMERIC 6,
+  RIGHTCURLY
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,14 @@
-import Environment
-import Data.Char
+import IML.FrontEnd.Lexer
 
-listEnv env = map (get env)
+example = ["quo <- 0",
+           "rem <- 42",
+           "while(rem >= 6) {",
+           "  quo <- (quo + 1)",
+           "  rem <- (rem - 6)",
+           "}"]
 
-fstIds = ['a', 'b', 'c']
-sndIds = ['d', 'e', 'f']
-
-fstVals = [2,  42, 21]
-sndVals = [8, -23, 18]
-
-fns   = [(*2), (+ 4), subtract 3]
-
-fstEnv = foldl set mempty $ zip fstIds fstVals
-sndEnv = foldl set mempty $ zip sndIds sndVals
-thdEnv = foldl set mempty $ zip fstIds sndVals
-fnsEnv = foldl set mempty $ zip fstIds fns
-
--- Functor demo
-doubledEnv = fmap (*2) fstEnv
-
--- Applicative demo
-funcedEnv   = fnsEnv <*> fstEnv
-funcedEnv'  = fnsEnv <*> sndEnv
-funcedEnv'' = pure (*2) <*> fstEnv
-
--- Monoid demo
-appendedEnv = fstEnv `mappend` sndEnv
-appendedEnv' = mconcat [fstEnv, sndEnv, thdEnv]
-
--- Monad demo
-wrappedEnv = sndEnv >>= pure
-wrappedEnv' = sndEnv >>= idToInt
-  where idToInt _ = E $ \x -> Just $ ord x
+main = do
+  putStrLn "Current program:"
+  putStrLn $ unlines example
+  putStrLn "\nToken Stream:"
+  print $ tokenize $ unlines example
