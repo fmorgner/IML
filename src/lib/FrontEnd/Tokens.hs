c9d283a6561184155b0af01e0618b2a8b8df796f
Felix Morgner <felix.morgner@gmail.com>
2017-07-07 21:43:14 +0200

    frontend: add token definitions

---
+++ src/lib/FrontEnd/Tokens.hs
@@ -0,0 +1,38 @@
+module Tokens where
+
+import MiddleEnd.Syntax
+import Data.Maybe
+
+data Token = Token {getTerminal  :: Terminal,
+                    getAttribute :: Maybe Attribute}
+
+data Terminal
+  = NUMERIC
+  | BOOLEAN
+  | STRING
+  | IDENTIFIER
+  | NOOP
+  | BECOMES
+  | LEFTCURLY
+  | RIGHTCURLY
+  | LEFTPAREN
+  | RIGHTPAREN
+  | IF
+  | ELSE
+  | WHILE
+  | FOR
+  | TO
+  | COMMA
+  | BINARARITHMETICOPERATOR
+  | BINARYBOOLEANOPERATOR
+  | RELATIONALOPERATOR
+  deriving (Eq, Show)
+
+data Attribute
+  = ArithmeticValue Integer
+  | BooleanValue Bool
+  | Name String
+  | ArithmeticOperator {getOperator :: IMLArithmeticOperator}
+  | BooleanOperator {getOperator :: IMLBooleanOperator}
+  | RelationalOperator {getOperator :: IMLRelationalOperator}
+  deriving (Eq, Show)
